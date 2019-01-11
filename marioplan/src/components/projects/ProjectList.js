a8b5849d58aefacac630cb892125982d1abf414c
Andrzej Ruga <andrzej@ruga.pl>
2019-01-11 15:00:57 +0100

    Added Logging user in

--- marioplan/src/components/projects/ProjectList.js
+++ marioplan/src/components/projects/ProjectList.js
@@ -7,8 +7,8 @@ const ProjectList = ({projects}) => {
         <div className="project-list section">
             { projects && projects.map(project => { // if we have projects, do the 2nd part
                 return (
-                    <Link to={'/project/' + project.id}>
-                        <ProjectSummary project={project} key={project.id} />
+                    <Link to={'/project/' + project.id} key={project.id}>
+                        <ProjectSummary project={project} />
                     </Link>
                 )
             })}
--- marioplan/src/store/reducers/rootReducer.js
+++ marioplan/src/store/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import authReducer from './authReducer';
 import projectReducer from './projectReducer';
 import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore';
+import { firebaseReducer } from 'react-redux-firebase';
 
 const rootReducer = combineReducers({
     auth: authReducer,
     project: projectReducer,
-    firestore: firestoreReducer
+    firestore: firestoreReducer, // by this, we sync firestore with our state
+    firebase: firebaseReducer // we sync firebase information, including authentication
 });
 
 export default rootReducer;
