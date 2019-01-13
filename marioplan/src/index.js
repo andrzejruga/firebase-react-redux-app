58b7a76572aa8c8f94db52a413a8a8a0acff1751
Andrzej Ruga <andrzej@ruga.pl>
2019-01-13 14:30:07 +0100

    Added loading proper links in Navbar, whether user is logged in or logged out

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -15,13 +15,19 @@ const store = createStore(rootReducer,
     compose(
         applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
         reduxFirestore(fbConfig), // store enhancer
-        reactReduxFirebase(fbConfig) // store enhancer
+        reactReduxFirebase(fbConfig, {attachAuthIsReady: true}) // store enhancer
     )
 ); // we're passing root reducer that combines single reducers into one
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// thanks to attachAuthIsReady and this below we're not rendering this to the DOM
+// until firebase auth is ready, so we don't see login and signin links for a few seconds
+// each time we refresh the page while logged in
+store.firebaseAuthIsReady.then(() => {
+    ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: http://bit.ly/CRA-PWA
+    serviceWorker.unregister();
+})
+
