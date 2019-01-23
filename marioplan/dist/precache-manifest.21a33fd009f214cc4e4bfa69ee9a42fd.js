a5f0dd40b6816c4e019e08c231e7b1f2477dafce
Andrzej Ruga <andrzej@ruga.pl>
2019-01-23 15:04:07 +0100

    Deployed app to firebase hosting

---
+++ marioplan/dist/precache-manifest.21a33fd009f214cc4e4bfa69ee9a42fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9eeda260d1a7197e71b6",
+    "url": "/static/js/main.9eeda260.chunk.js"
+  },
+  {
+    "revision": "47ad9d9874bea38058b3",
+    "url": "/static/js/1.47ad9d98.chunk.js"
+  },
+  {
+    "revision": "9eeda260d1a7197e71b6",
+    "url": "/static/css/main.35226a0b.chunk.css"
+  },
+  {
+    "revision": "6088c54be9eaa63a2fa6ae1f41205999",
+    "url": "/index.html"
+  }
+];
